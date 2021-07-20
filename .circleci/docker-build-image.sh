9074735f99889631c4acacc8113ca9f55c5638ee
Sujata141180 <87296986+Sujata141180@users.noreply.github.com>
2021-07-20 17:53:37 +0530

    docker login -u sujata8073

--- .circleci/docker-build-image.sh
+++ .circleci/docker-build-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build -t mycircleci:3 -f .circleci/Dockerfile .
-docker login
+docker login -u sujata8073
 docker tag mycircleci:3 sujata8073/mycircleci
 docker push sujata8073/mycircleci
