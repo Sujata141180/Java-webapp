4bd96866213d545fae8b162d064f4d6fdc81b419
Sujata141180 <87296986+Sujata141180@users.noreply.github.com>
2021-07-20 19:17:57 +0530

    added username password

--- .circleci/docker-build-image.sh
+++ .circleci/docker-build-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build -t mycircleci:latest -f .circleci/Dockerfile .
-docker login -u sujata8073
+docker login -u sujata8073 -p Sujata@8073
 docker tag mycircleci:latest sujata8073/circlecipractice:circlecii
 docker push sujata8073/circlecipractice:circlecii
