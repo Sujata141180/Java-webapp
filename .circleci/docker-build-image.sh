cdc5db75ffba19393430518525e84bbce874d591
Sujata141180 <87296986+Sujata141180@users.noreply.github.com>
2021-07-20 19:02:11 +0530

    edited push file

--- .circleci/docker-build-image.sh
+++ .circleci/docker-build-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build -t mycircleci:3 -f .circleci/Dockerfile .
+docker build -t mycircleci:latest -f .circleci/Dockerfile .
 docker login -u sujata8073
-docker tag mycircleci:3 sujata8073/mycircleci
-docker push sujata8073/mycircleci
+docker tag mycircleci:latest sujata8073/circlecipractice:circlecii
+docker push sujata8073/circlecipractice:circlecii
