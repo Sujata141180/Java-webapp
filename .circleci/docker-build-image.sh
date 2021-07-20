bb8168e51a62a96da94d415116512eb1863615ce
Sujata141180 <87296986+Sujata141180@users.noreply.github.com>
2021-07-20 17:20:26 +0530

    edited build command

--- .circleci/docker-build-image.sh
+++ .circleci/docker-build-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t mycircleci:3 --build-arg --file .circleci/Dockerfile .
+docker build -t mycircleci:3 --build-arg --file .
 docker login
 docker push sujata8073/mycircleci:3
